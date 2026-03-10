51ae474010414ab24d8cef014fbaa5a70a1f08f6
JohnDupartDev <johndupartdev@gmail.com>
2026-03-10 10:47:47 -0500

    import images public

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,92 +1,93 @@
 import Slider from './slider'
 import { Link } from "react-router-dom"
-import logoNavegante from "../assets/image/logonavegante.svg";
 
-
-import { FaSignInAlt,FaBookReader, FaCommentDollar, FaUsers, FaMapMarkedAlt} from 'react-icons/fa';
+// IMPORTANTE: Eliminamos el import de assets y usamos la ruta de public directamente
+import { FaSignInAlt, FaBookReader, FaCommentDollar, FaUsers, FaMapMarkedAlt } from 'react-icons/fa';
 
 export default function index() {
-  // Importar iconos de react-icons
+  // Definimos la ruta del logo apuntando a la carpeta public
+  const logoNavegantePath = "/images/logonavegante.svg";
 
- 
   return (
     <div id="welcomeModal">
-        <div id="modalContent">
-     
-          <div className="contenar">
-           
-            <a className="logo" href='https://navegantesonoro.com/'>
-          <img className='imglogo'            
-              src={logoNavegante}
-         alt='logo_zipaquiardigital' rel="noopener noreferrer"/>
+      <div id="modalContent">
+        <div className="contenar">
+          <a className="logo" href='https://navegantesonoro.com/'>
+            <img 
+              className='imglogo'            
+              src={logoNavegantePath} // Usamos la ruta directa de public
+              alt='logo_zipaquiardigital' 
+              rel="noopener noreferrer"
+            />
           </a>
         
-              <p className="textline">Una experiencia única 
-                donde los sonidos cuentan historias. 
-              </p>
-              <div className="contenedorbotones">
-            
-      <div className="contenedor">
-    
-        {/* Botones con iconos */}
-        <div className="boton">
-        <div className='iconhome'>
-        <FaSignInAlt />
-       </div>
-       <div className='icontext'>
-        <Link id="startton" to="/micuenta">
-            <span>Mi Ruta</span>
-          </Link>
-          </div>
-        </div>
-        <div className="boton">
-        <div className='iconhome'>
-        <FaCommentDollar />
-       </div>
-          <Link id="startton" to="/comprar">
-          
-            <span>Comprar</span>
-          </Link>
-        </div>
-        <div className="boton">
-        <div className='iconhome'>
-        <FaBookReader />
-       </div>
-          <Link id="startton" to="/comofunciona">
-           
-            <span>¿Como funciona?</span>
-          </Link>
-        </div>
-        <div className="boton">
-        <div className='iconhome'>
-        <FaUsers />
-       </div>
-          <Link id="startton" to="/aliados">
-           
-            <span>Aliados</span>
-          </Link>
-        </div>
-        <div className="boton">
-        <div className='iconhome'>
-        <FaMapMarkedAlt />
-       </div>
-          <Link id="startton" to="/mapa">
-           
-            <span>Mapa</span>
-          </Link>
-        </div>
-        <Slider/>
-      </div>
-    </div> 
-            <div className="zip">
-              <p className="text">®Desarrollado por Zipaquirá Digital</p>
-              <div className="contenedorlink">
-              <a className="link"  href="https://wa.me/573195301551?text=Hola,%20Me%20gustaria%20saber%20mas%20sobre%20los%20servicios%." target='blank' > Contacto </a>
-              <a className="link"  href="https://zipaquiradigital.com/tarjetas-de-presentacion-con-realidad-aumentada-ar/" target='blank' > Página Web </a>
+          <p className="textline">
+            Una experiencia única donde los sonidos cuentan historias. 
+          </p>
+
+          <div className="contenedorbotones">
+            <div className="contenedor">
+              {/* Botones con iconos */}
+              <div className="boton">
+                <div className='iconhome'>
+                  <FaSignInAlt />
+                </div>
+                <div className='icontext'>
+                  <Link id="startton" to="/micuenta">
+                    <span>Mi Ruta</span>
+                  </Link>
+                </div>
               </div>
+
+              <div className="boton">
+                <div className='iconhome'>
+                  <FaCommentDollar />
+                </div>
+                <Link id="startton" to="/comprar">
+                  <span>Comprar</span>
+                </Link>
+              </div>
+
+              <div className="boton">
+                <div className='iconhome'>
+                  <FaBookReader />
+                </div>
+                <Link id="startton" to="/comofunciona">
+                  <span>¿Como funciona?</span>
+                </Link>
+              </div>
+
+              <div className="boton">
+                <div className='iconhome'>
+                  <FaUsers />
+                </div>
+                <Link id="startton" to="/aliados">
+                  <span>Aliados</span>
+                </Link>
+              </div>
+
+              <div className="boton">
+                <div className='iconhome'>
+                  <FaMapMarkedAlt />
+                </div>
+                <Link id="startton" to="/mapa">
+                  <span>Mapa</span>
+                </Link>
+              </div>
+              
+              <Slider/>
+            </div>
+          </div> 
+
+          <div className="zip">
+            <p className="text">®Desarrollado por Zipaquirá Digital</p>
+            <div className="contenedorlink">
+              <a className="link" href="https://wa.me/573195301551?text=Hola,%20Me%20gustaria%20saber%20mas%20sobre%20los%20servicios%." target='blank'> Contacto </a>
+              <a className="link" href="https://zipaquiradigital.com/tarjetas-de-presentacion-con-realidad-aumentada-ar/" target='blank'> Página Web </a>
             </div>
           </div>
         </div>
       </div>
+    </div>
   )
 }
--- src/routes/slider.jsx
+++ src/routes/slider.jsx
@@ -3,19 +3,12 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import { Link } from 'react-router-dom';
 import { FaChevronLeft, FaChevronRight, FaRoute } from 'react-icons/fa'; // Importa los iconos
-import '../index.css'; // Archivo CSS donde defines la clase .blurred
-
-// IMPORTACIÓN DE IMÁGENES
-import estacionImg from '../assets/image/estacion.jpg';
-import casanavasImg from '../assets/image/casanavas.jpg';
-import plazaImg from '../assets/image/plaza.jpg';
-import independenciaImg from '../assets/image/independencia.jpg';
-import funzipaImg from '../assets/image/funzipa.jpg';
-import catedralImg from '../assets/image/catedral.jpg';
-import audioIcon from '../assets/image/audio.svg';
-import dineroIcon from '../assets/image/dinero.png';
+import '../index.css'; // Archivo CSS donde tienes .blurred
 
 export default function Slider() {
+  // Definimos las rutas constantes apuntando a la carpeta public/images/
+  const path = "/images/";
+
   const aplicarDesenfoque = () => {
     const elementosDesenfocados = document.querySelectorAll('.imglogo, #startButton, .textline, .aliados, .bienvenidos');
     elementosDesenfocados.forEach(elemento => {
@@ -69,13 +62,16 @@ export default function Slider() {
 
               <div id="carouselExampleIndicators" className="carousel slide">
                 <div className="carousel-inner">
+                  {/* Estación 1 */}
                   <div className="carousel-item active">
                     <div className="card text-white">
-                      <img src={estacionImg} className="card-img-top" alt="Estación Tres Esquinas" />
+                      <img src={`${path}estacion.jpg`} className="card-img-top" alt="Estación Tres Esquinas" />
                       <div className="card-body">
                         <h5 className="card-title ">CAPÍTULO 1. ESTACIÓN DEL FERROCARRIL TRES ESQUINAS</h5>
                         <p className="card-text">Luisa es obligada por su familia a viajar a Zipaquirá. A su llegada, siente una atracción por un artista que vio en la Estación del Tren. </p>
-                        <Link to="https://navegantesonoro.com/estaciondeltren" className="btn btn-outline-warning"> Escuchar <img src={audioIcon} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" /></Link>
+                        <Link to="https://navegantesonoro.com/estaciondeltren" className="btn btn-outline-warning"> 
+                          Escuchar <img src={`${path}audio.svg`} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" />
+                        </Link>
                       </div>
                     </div>
                   </div>
@@ -83,11 +79,13 @@ export default function Slider() {
                   {/* Estación 2 */}
                   <div className="carousel-item">
                     <div className="card text-white">
-                      <img src={casanavasImg} className="card-img-top" alt="Estación Candelaria" />
+                      <img src={`${path}casanavas.jpg`} className="card-img-top" alt="Estación Candelaria" />
                       <div className="card-body">
                         <h5 className="card-title">CAPÍTULO 2. CASA DE LAS NAVAS</h5>
                         <p className="card-text">Antonio se dirige al encuentro con sus amigos en el centro de Zipaquirá, y demuestra su tristeza por no haber podido hablar con la mujer de la Estación del Tren.</p>
-                        <Link to="https://navegantesonoro.com/casanavas" className="btn btn-outline-warning"> Escuchar <img src={audioIcon} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" /></Link>
+                        <Link to="https://navegantesonoro.com/casanavas" className="btn btn-outline-warning"> 
+                          Escuchar <img src={`${path}audio.svg`} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" />
+                        </Link>
                       </div>
                     </div>
                   </div>
@@ -95,11 +93,13 @@ export default function Slider() {
                   {/* Estación 3 */}
                   <div className="carousel-item">
                     <div className="card text-white">
-                      <img src={plazaImg} className="card-img-top bgcover" alt="Estación del Sur" />
+                      <img src={`${path}plaza.jpg`} className="card-img-top bgcover" alt="Estación del Sur" />
                       <div className="card-body">
                         <h5 className="card-title">CAPÍTULO 3. PLAZA PRINCIPAL</h5>
                         <p className="card-text">Llega el domingo y con él la Santa Misa y la retreta ambientada en este lugar. Antonio está listo para tocar con la Banda Municipal. Las sorpresas no se harán esperar.</p>
-                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">Comprar<img src={dineroIcon} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" /></Link>
+                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">
+                          Comprar<img src={`${path}dinero.png`} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" />
+                        </Link>
                       </div>
                     </div>
                   </div>
@@ -107,11 +107,13 @@ export default function Slider() {
                   {/* Estación 4 */}
                   <div className="carousel-item">
                     <div className="card text-white">
-                      <img src={independenciaImg} className="card-img-top" alt="Estación Central" />
+                      <img src={`${path}independencia.jpg`} className="card-img-top" alt="Estación Central" />
                       <div className="card-body">
                         <h5 className="card-title">CAPÍTULO 4. PLAZA DE LA INDEPENDENCIA E IGLESIA DE NUESTRA SEÑORA DE LOS DOLORES</h5>
                         <p className="card-text">Todo está listo para el reencuentro entre Luisa y Antonio en el punto más alto del centro histórico de Zipaquirá.</p>
-                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">Comprar<img src={dineroIcon} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" /></Link>
+                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">
+                          Comprar<img src={`${path}dinero.png`} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" />
+                        </Link>
                       </div>
                     </div>
                   </div>
@@ -119,11 +121,13 @@ export default function Slider() {
                   {/* Estación 5 */}
                   <div className="carousel-item">
                     <div className="card text-white">
-                      <img src={funzipaImg} className="card-img-top" alt="Estación El Dorado" />
+                      <img src={`${path}funzipa.jpg`} className="card-img-top" alt="Estación El Dorado" />
                       <div className="card-body">
                         <h5 className="card-title">CAPÍTULO 5. ANTIGUO HORNO TRADICIONAL DE SAL</h5>
                         <p className="card-text">Luisa no puede escapar de los acontecimientos políticos y económicos que vive Zipaquirá. Tendrá que decidir entre sus dos amores.</p>
-                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">Comprar<img src={dineroIcon} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" /></Link>
+                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">
+                          Comprar<img src={`${path}dinero.png`} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" />
+                        </Link>
                       </div>
                     </div>
                   </div>
@@ -131,15 +135,18 @@ export default function Slider() {
                   {/* Estación 6 */}
                   <div className="carousel-item">
                     <div className="card text-white">
-                      <img src={catedralImg} className="card-img-top" alt="Estación del Norte" />
+                      <img src={`${path}catedral.jpg`} className="card-img-top" alt="Estación del Norte" />
                       <div className="card-body">
                         <h5 className="card-title">CAPÍTULO 6. ALREDEDORES PARQUE DE LA SAL</h5>
                         <p className="card-text">La verdad es revelada. Luisa y Antonio se enfrentan a sus mayores miedos.</p>
-                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">Comprar<img src={dineroIcon} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" /></Link>
+                        <Link to="https://navegantesonoro.com/comprar" className="btn btn-outline-warning">
+                          Comprar<img src={`${path}dinero.png`} style={{ width: '20px', height: '20px', marginLeft: '0.5rem' }} alt="Icono" />
+                        </Link>
                       </div>
                     </div>
                   </div>
                 </div>
+
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
                   <FaChevronLeft style={{ color: 'white', width:'25px', height:'25px', background:'black', padding:'2px', borderRadius: '40px'}} /> 
                   <span className="visually-hidden">Previous</span>
