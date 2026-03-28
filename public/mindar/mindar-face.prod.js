9f22527b3e71b651d0e6525b4989982f3cd1579c
JohnDupartDev <johndupartdev@gmail.com>
2026-03-28 18:40:30 -0500

    Remover archivos pesados de public

--- public/mindar/mindar-face.prod.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:db35a7d60cbfe9393577a1da6da04e5de8837d35e17eea020bab627e6c5ba57a
-size 1396296
--- public/mindar/mindar-image-aframe.prod.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1cc6462e8e76278992ee522294b2334e8552828a21d75d489533a230d7402f37
-size 6110
